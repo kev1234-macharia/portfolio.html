b0007190992abf5d5c8cf2a41902ebcdff9ce07d
Keam Keymoney <124783956+kev1234-macharia@users.noreply.github.com>
2024-03-20 15:17:16 +0300

    FIRST COMMIT

---
+++ my_list.py
@@ -0,0 +1,19 @@
+my_list = []
+print("my_list:", my_list)
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print("my_list:", my_list)
+my_list.insert(1, 15)
+print(my_list) 
+my_list.extend([50, 60, 70])
+print("updated_list:" ,my_list)
+my_list.pop()
+print("updated_list:", my_list)
+my_list.sort()
+print("sorted_list:", my_list)
+index_of_30 = my_list.index(30)
+print("Updated_LIST:", my_list)
+
+
